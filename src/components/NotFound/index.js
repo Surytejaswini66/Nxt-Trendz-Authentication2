1097b8aa9f1a45e2ff8a71dee3e1f2910af53a72
Surytejaswini66 <suryaviswanadhapalli666@gmail.com>
2024-09-23 08:57:58 +0000

    solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-not-found-img.png"
+      alt="not found"
+    />
+    <h1>Page Not Found</h1>
+  </div>
+)
+
+export default NotFound
